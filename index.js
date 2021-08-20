86741b191b2fd131f400bff8a9111e33f010f383
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2021-08-20 12:56:51 -0400

    12

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ const zooAnimals = [
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a,b){
+   return a + b;
   }
 
 
